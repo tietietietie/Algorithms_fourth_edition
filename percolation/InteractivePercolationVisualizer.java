b834c7548414fec341145a6f73ad30146b61b8af
tietietietie <45412653+tietietietie@users.noreply.github.com>
2019-08-31 20:52:58 +0800

    update some codes of Precolation.java

--- percolation/InteractivePercolationVisualizer.java
+++ percolation/InteractivePercolationVisualizer.java
@@ -1,33 +1,32 @@
-/* *****************************************************************************
+/******************************************************************************
  *  Compilation:  javac InteractivePercolationVisualizer.java
  *  Execution:    java InteractivePercolationVisualizer n
  *  Dependencies: PercolationVisualizer.java Percolation.java
+ *                StdDraw.java StdOut.java
  *
  *  This program takes the grid size n as a command-line argument.
  *  Then, the user repeatedly clicks sites to open with the mouse.
  *  After each site is opened, it draws full sites in light blue,
  *  open sites (that aren't full) in white, and blocked sites in black.
  *
- **************************************************************************** */   
+ ******************************************************************************/
+
 import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
 
 public class InteractivePercolationVisualizer {
-    private static final int DELAY = 20;
 
     public static void main(String[] args) {
-        // n-by-n percolation system (read from command-line, default n = 10)
-        int n = 10;
+        // n-by-n percolation system (read from command-line, default = 10)
+        int n = 10;          
         if (args.length == 1) n = Integer.parseInt(args[0]);
 
-        // turn on animation mode
-        StdDraw.enableDoubleBuffering();
-
-        // repeatedly open site specified by mouse click and draw system
+        // repeatedly open site specified my mouse click and draw resulting system
         StdOut.println(n);
 
-        Percolation percolation = new Percolation(n);
-        PercolationVisualizer.draw(percolation, n);
+        StdDraw.enableDoubleBuffering();
+        Percolation perc = new Percolation(n);
+        PercolationVisualizer.draw(perc, n);
         StdDraw.show();
 
         while (true) {
@@ -39,24 +38,24 @@ public class InteractivePercolationVisualizer {
                 double x = StdDraw.mouseX();
                 double y = StdDraw.mouseY();
 
-                // convert to (row, col)
-                int row = (int) (n - Math.floor(y) - 1);
-                int col = (int) (Math.floor(x));
+                // convert to row i, column j
+                int i = (int) (n - Math.floor(y));
+                int j = (int) (1 + Math.floor(x));
 
-                // open site (i, col) provided it's in bounds
-                if (row >= 0 && row < n && col >= 0 && col < n) {
-                    if (!percolation.isOpen(row, col)) {
-                        StdOut.println(row + " " + col);
+                // open site (i, j) provided it's in bounds
+                if (i >= 1 && i <= n && j >= 1 && j <= n) {
+                    if (!perc.isOpen(i, j)) { 
+                        StdOut.println(i + " " + j);
                     }
-                    percolation.open(row, col);
+                    perc.open(i, j);
                 }
 
                 // draw n-by-n percolation system
-                PercolationVisualizer.draw(percolation, n);
+                PercolationVisualizer.draw(perc, n);
+                StdDraw.show();
             }
-            StdDraw.show();
-            StdDraw.pause(DELAY);
+
+            StdDraw.pause(20);
         }
     }
 }
-
--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -1,18 +1,34 @@
 /* *****************************************************************************
- *  Name:zhangtie
- *  Date:2019.8.30
- *  Description:Finish the class <Percolation>
+ *  Name:Iron zhang
+ *  Date:2019.8.31
+ *  Description:
  **************************************************************************** */
 
 public class Percolation {
+    private int[] site;
+    private int n;
+
     public Percolation(int n) {
-        if (n <= 0) throw new java.lang.IllegalArgumentException("n must be more than zero");
-        int[] row = new int[n];
-        int[] col = new int[n];
-        for (int i = 0; i < n; i++) {
-            row[i] = i + 1;
-            col[i] = i + 1;
+        if (n <= 0)
+            throw new java.lang.IllegalArgumentException("n must be more than zero");
+        site = new int[n * n + 2];
+        site[0] = 1;
+        site[n * n + 1] = n * n + 1;
+        for (int i = 1; i < n + 1; i++) {
+            site[i] = 0;
         }
+        for (int i = (n - 2) * n + 1; i < (n - 1) * n + 1; i++) {
+            site[i] = i;
+        }
+        for (int i = (n - 1); i < n * n + 1; i++) {
+            site[i] = n * n + 1;
+        }
+    }
+
+    public void open(int row, int col) {
+        int pos = (row - 1) + 1;
+        if (pos < n + 1) ;
+
     }
 
     public static void main(String[] args) {
