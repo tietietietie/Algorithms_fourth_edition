d2860990427c2e325fd367fb5c5a92004fb02edd
ZhangTie <zhangtie@hust.edu.cn>
2019-09-18 23:12:43 +0800

    code

---
+++ Code/LinkedQueueOfStrings.java
@@ -0,0 +1,31 @@
+public class LinkedQueueOfStrings
+{
+    private Node first, last;
+
+    Private class Node
+    {
+        private String item;
+        private Node next;
+    }
+
+    public boolean isEmpty()
+    { return first == null;}
+
+    public void enqueue(String item)
+    {
+        Node oldlast = last;
+        last = new Node();
+        last.item = item;
+        last.next = null;
+        if(isEmpty()) first = last;
+        else oldlast.next = last;
+    }
+
+    public String dequeue()
+    {
+        String item  = first.item;
+        first = first.next;
+        if(isEmpty()) last = null;
+        return item;
+    }
+}
