75b211b09cd89f69cce399935cb525a2c54e8323
ZhangTie <zhangtie@hust.edu.cn>
2019-09-23 14:52:00 +0800

    Quciksort/QucikSelect

--- Code/QuickSelect.java
+++ Code/QuickSelect.java
@@ -1,5 +1,20 @@
-public class Quick
+public class QuickSelect
 {
+    public static Comparable select(Comparable[] a, int k)
+    {
+        StdRandom.shuffle(a);
+        int lo = 0, hi = a.length-1;
+        while(lo <= hi)
+        {
+            int j = partition(a, lo, hi);
+            if(j < k) lo = j+1;
+            else if (j > k) hi = j-1;
+            else return a[k];
+        }
+
+        return a[k];  //??
+    }
+
     private static int partition(Comparable[] a, int lo, int hi) {
         int i = lo, j = hi+1;
         while(true)
@@ -15,19 +30,5 @@ public class Quick
         }
         exch(a, lo, j);
         return j;
-    }
-
-    private static void sort(Comparable[] a, int lo, int hi)
-    {
-        if(lo >= hi) return;
-        int j = partition(a, lo, hi);
-        sort(a, lo, j-1);
-        sort(a, j+1, hi);
-    }
-
-    public static void sort(Comparable[] a)
-    {
-        StdRandom.shuffle(a);
-        sort(a, 0, a.length-1);
     }
 }
